1ac3eee12a95e5931b0020a1e206bb727d457f30
ian <ian.page38@gmail.com>
2026-02-27 14:08:13 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "NixOS Flake for OpenClaw NUC";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-openclaw.url = "github:openclaw/nix-openclaw";
